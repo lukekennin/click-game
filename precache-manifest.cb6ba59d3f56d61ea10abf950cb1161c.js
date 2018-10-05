b7d040b70eafd9e1cb59ea247e7e40b14b0ff463
Luke Kennin <lkmeq@me.com>
2018-10-04 20:10:04 -0500

    Updates

---
+++ precache-manifest.cb6ba59d3f56d61ea10abf950cb1161c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "451cb8796073a9e8640b",
+    "url": "/click-game/static/js/runtime~main.451cb879.js"
+  },
+  {
+    "revision": "c00f72b0c4f9358a41dc",
+    "url": "/click-game/static/js/main.c00f72b0.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/click-game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c00f72b0c4f9358a41dc",
+    "url": "/click-game/static/css/main.139b1ec5.chunk.css"
+  },
+  {
+    "revision": "5e4ed45d16f2a73e409b2596e4cd485b",
+    "url": "/click-game/index.html"
+  }
+];
